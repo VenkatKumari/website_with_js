63146b69da74c366acfb5bb54d10818e705d08ca
VenkatKumari <venki.kumari@gmail.com>
2020-09-28 09:44:01 +0200

    All animations done

---
+++ experiment.js
@@ -0,0 +1,12 @@
+// const hikeExp = document.querySelector(".hike-explore");
+
+// window.addEventListener("scroll", scrollReveal);
+
+// function scrollReveal() {
+//   const hikePos = hikeExp.getBoundingClientRect().top;
+//   const windowHeight = window.innerHeight / 1.5;
+
+//   if (hikePos < windowHeight) {
+//     hikeExp.style.color = "red";
+//   }
+// }
